3295399215f4a5aeb061a7824c8ee0a06b5ed1e6
Raphael <raphaelareas@MacBook-Air-Raphael-Kyte.local>
2026-01-18 18:18:05 -0300

    fix: Remove import não utilizado do UpcomingExpensesWidget

--- src/components/dashboard/UpcomingExpensesWidget/UpcomingExpensesWidget.tsx
+++ src/components/dashboard/UpcomingExpensesWidget/UpcomingExpensesWidget.tsx
@@ -1,5 +1,3 @@
-import { formatCurrency } from '@/utils/formatCurrency';
-
 const WalletIcon = () => (
   <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
     <rect x="2" y="4" width="16" height="12" rx="2" stroke="currentColor" strokeWidth="2"/>
